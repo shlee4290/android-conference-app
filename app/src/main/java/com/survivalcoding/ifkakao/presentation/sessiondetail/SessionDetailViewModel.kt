1621a2fce942c64d5eed5f243628f9d0d399a721
peter.shlee <peter.shlee@kakaocorp.com>
2022-01-26 18:57:04 +0900

    Proto datastore로 좋아요 누른 세션 저장하는 기능 구현

--- app/src/main/java/com/survivalcoding/ifkakao/presentation/sessiondetail/SessionDetailViewModel.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/presentation/sessiondetail/SessionDetailViewModel.kt
@@ -2,7 +2,8 @@ package com.survivalcoding.ifkakao.presentation.sessiondetail
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class SessionDetailViewModel: ViewModel() {
+class SessionDetailViewModel @Inject constructor(): ViewModel() {
 }
