19afbde337a229fcb6c653693219054713abde02
SeungHyun Lee <shlee4290@naver.com>
2022-02-02 00:29:29 +0900

    세션 영상 웹브라우저에서 볼 수 있도록 연결

--- app/src/main/java/com/survivalcoding/ifkakao/presentation/sessiondetail/SessionDetailFragment.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/presentation/sessiondetail/SessionDetailFragment.kt
@@ -1,5 +1,7 @@
 package com.survivalcoding.ifkakao.presentation.sessiondetail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -84,7 +86,11 @@ class SessionDetailFragment : Fragment() {
     }
 
     private fun navigateToWebView(url: String) {
-
+        Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
+            if (resolveActivity(requireActivity().packageManager) != null) {
+                requireActivity().startActivity(this)
+            }
+        }
     }
 
     private fun navigateToSessionList() {
