5cd5dfbd81cdebe4f3ab59df0faa02fa783b7bad
SeungHyun Lee <shlee4290@naver.com>
2022-02-01 23:23:11 +0900

    SessionDetailFragment 화면에 표시될 item들을 생성하는 방식 수정

--- app/src/main/java/com/survivalcoding/ifkakao/presentation/sessiondetail/SessionDetailViewModel.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/presentation/sessiondetail/SessionDetailViewModel.kt
@@ -8,7 +8,9 @@ import com.survivalcoding.ifkakao.domain.usecase.GetSelectedSessionsUseCase
 import com.survivalcoding.ifkakao.presentation.common.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -18,7 +20,10 @@ class SessionDetailViewModel @Inject constructor(
     private val getSelectedSessionsUseCase: GetSelectedSessionsUseCase
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow(SessionDetailUiState(listOf()))
+    private val _eventFlow = MutableSharedFlow<Event>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    private val _uiState = MutableStateFlow(UiState(listOf()))
     val uiState = _uiState.asStateFlow()
 
     private var session: Session? = null
@@ -30,53 +35,144 @@ class SessionDetailViewModel @Inject constructor(
 
     private fun refreshBinderList(session: Session) {
         viewModelScope.launch {
-            val fieldCategory = if (session.category.field.isNotEmpty()) {
-                session.category.field.first()
-            } else {
-                ""
-            }
-
-            val relatedSessions = async {
-                getSelectedSessionsUseCase(category = Category(field = listOf(fieldCategory)))
-            }
-
-            val tmpBinderList = mutableListOf<CommonBinder>()
-
-            if (session.video.isNotEmpty()) {
-                tmpBinderList.add(
-                    SessionVideoBinder(
-                        session.video.first(),
-                        session.thumbnailUrl,
-                        session.title,
-                        {},// TODO
-                        {}// TODO
-                    )
-                )
-            }
-            tmpBinderList.add(CategoryListBinder(session.category))
-            tmpBinderList.add(SessionTitleBinder(session.title))
-            tmpBinderList.add(SessionContentBinder(session.content))
-            tmpBinderList.add(SessionTagsBinder(session.contentTag))
-            tmpBinderList.addAll(
-                session.contentsSpeakers.map {
-                    SpeakerBinder(it)
-                }
-            )
-            tmpBinderList.add(SessionButtonBinder("목록보기") {}) // TODO
-            tmpBinderList.add(AssociatedSessionTitleBinder())
-            tmpBinderList.addAll(
-                relatedSessions.await().filter { it.idx != session.idx }
-                    .map { SessionListItemBinder(it, {}) } // TODO
-            )
-            tmpBinderList.add(SessionButtonBinder("연관세션 더보기") {}) // TODO
-            tmpBinderList.add(FooterBinder())
-
-            _uiState.value = _uiState.value.copy(binderList = tmpBinderList)
+            _uiState.value = _uiState.value.copy(binderList = buildSessionDetailBinderList(session))
         }
     }
 
+    private suspend fun buildSessionDetailBinderList(session: Session): List<CommonBinder> {
+        val fieldCategory = if (session.category.field.isNotEmpty()) {
+            session.category.field.first()
+        } else {
+            ""
+        }
+
+        val relatedSessions = viewModelScope.async {
+            getSelectedSessionsUseCase(category = Category(field = listOf(fieldCategory)))
+        }
+
+        return SessionDetailBinderListBuilder()
+            .addVideo(
+                session, { url -> sendEvent(Event.NavigateToWebView(url)) },
+                {} // TODO
+            ).addCategory(session)
+            .addTitle(session)
+            .addContent(session)
+            .addTags(session)
+            .addSpeakers(session)
+            .addButton("목록보기") { sendEvent(Event.NavigateToSessionList) }
+            .addAssociatedSessions(relatedSessions.await(), session) { clickedSession ->
+                sendEvent(
+                    Event.NavigateToSessionDetail(
+                        clickedSession
+                    )
+                )
+            }
+            .addButton("연관세션 더보기") {
+                sendEvent(
+                    Event.NavigateToAssociatedSessionList(
+                        Category(field = listOf(fieldCategory)), fieldCategory
+                    )
+                )
+            }.addFooter()
+            .build()
+    }
+
+    private fun sendEvent(event: Event) {
+        viewModelScope.launch {
+            _eventFlow.emit(event)
+        }
+    }
+
+    sealed class Event {
+        class NavigateToWebView(url: String) : Event()
+        class NavigateToSessionDetail(session: Session) : Event()
+        object NavigateToSessionList : Event()
+        class NavigateToAssociatedSessionList(category: Category, title: String) : Event()
+    }
+
+    data class UiState(
+        val binderList: List<CommonBinder>
+    )
 }
 
-data class SessionDetailUiState(
-    val binderList: List<CommonBinder>
-)
+class SessionDetailBinderListBuilder {
+
+    private val tmpBinderList = mutableListOf<CommonBinder>()
+
+    fun build(): List<CommonBinder> = tmpBinderList
+
+    fun addVideo(
+        session: Session,
+        onPlayButtonClick: (String) -> Unit,
+        onShareButtonClick: () -> Unit
+    ): SessionDetailBinderListBuilder {
+        if (session.video.isNotEmpty()) {
+            tmpBinderList.add(
+                SessionVideoBinder(
+                    session.video.first(),
+                    session.thumbnailUrl,
+                    session.title,
+                    onPlayButtonClick,
+                    onShareButtonClick
+                )
+            )
+        }
+        return this
+    }
+
+    fun addCategory(session: Session): SessionDetailBinderListBuilder {
+        tmpBinderList.add(CategoryListBinder(session.category))
+        return this
+    }
+
+    fun addTitle(session: Session): SessionDetailBinderListBuilder {
+        tmpBinderList.add(SessionTitleBinder(session.title))
+        return this
+    }
+
+    fun addContent(session: Session): SessionDetailBinderListBuilder {
+        tmpBinderList.add(SessionContentBinder(session.content))
+        return this
+    }
+
+    fun addTags(session: Session): SessionDetailBinderListBuilder {
+        tmpBinderList.add(SessionTagsBinder(session.contentTag))
+        return this
+    }
+
+    fun addSpeakers(session: Session): SessionDetailBinderListBuilder {
+        tmpBinderList.addAll(
+            session.contentsSpeakers.map {
+                SpeakerBinder(it)
+            }
+        )
+        return this
+    }
+
+    fun addAssociatedSessions(
+        associatedSessionList: List<Session>,
+        currentSession: Session,
+        onSessionClick: (Session) -> Unit
+    ): SessionDetailBinderListBuilder {
+        tmpBinderList.add(AssociatedSessionTitleBinder())
+        tmpBinderList.addAll(
+            associatedSessionList.filter { it.idx != currentSession.idx }
+                .map {
+                    SessionListItemBinder(
+                        it, onSessionClick
+                    )
+                }
+        )
+        return this
+    }
+
+    fun addButton(text: String, onClick: () -> Unit): SessionDetailBinderListBuilder {
+        tmpBinderList.add(SessionButtonBinder(text, onClick))
+        return this
+    }
+
+    fun addFooter(): SessionDetailBinderListBuilder {
+        tmpBinderList.add(FooterBinder())
+        return this
+    }
+}
