0afb44e71a65b1519e1b82e863deac0903bf3162
peter.shlee <peter.shlee@kakaocorp.com>
2022-02-04 17:35:08 +0900

    카카오 링크 API 사용 설정

--- app/src/main/java/com/survivalcoding/ifkakao/core/IfKakaoApplication.kt
+++ app/src/main/java/com/survivalcoding/ifkakao/core/IfKakaoApplication.kt
@@ -2,6 +2,8 @@ package com.survivalcoding.ifkakao.core
 
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
+import com.kakao.sdk.common.KakaoSdk
+import com.survivalcoding.ifkakao.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -9,5 +11,6 @@ class IfKakaoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        KakaoSdk.init(this, BuildConfig.KAKAO_LINK_NATIVE_APP_KEY)
     }
 }
